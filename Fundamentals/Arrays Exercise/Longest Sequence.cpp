487627baa1da9bf80fe1b7497b0fbf6e2c390db4
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2024-12-02 00:09:32 +0200

    Create Longest Sequence.cpp

---
+++ Fundamentals/Arrays Exercise/Longest Sequence.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using std::string;
+using std::vector;
+using std::cin;
+using std::cout;
+
+
+int main() {
+    std::vector<std::pair<int, int> > sequences;
+
+    int arrSize;
+    std::cin >> arrSize;
+
+    int *numbers = new int[arrSize];
+    // initializing the array of numbers
+    for (int i = 0; i < arrSize; ++i)
+        std::cin >> numbers[i];
+
+    int currentSequenceNumber = numbers[0], sequenceNumber = 1, biggestSequence = 1;
+
+    for (int i = 1; i < arrSize; ++i) {
+        if (numbers[i] == currentSequenceNumber) {
+            // if the current number is = to the previous sequence number
+            sequenceNumber++;
+
+            if (sequenceNumber > biggestSequence) // keep track of the biggest sequence
+                biggestSequence = sequenceNumber;
+
+        } else {
+            sequences.push_back({currentSequenceNumber, sequenceNumber}); // save the state of the current sequence
+
+            // reset the values
+            currentSequenceNumber = numbers[i];
+            sequenceNumber = 1;
+        }
+    }
+    // save the last sequence state
+    sequences.push_back({currentSequenceNumber, sequenceNumber});
+
+    for (int i = sequences.size() - 1; i > -1; --i) {
+        if (sequences.at(i).second == biggestSequence) {
+            // if the current sequence = biggest sequence and it's the last in line
+            for (int j = 0; j < biggestSequence; ++j)
+                std::cout << sequences.at(i).first << ' ';
+
+            break;
+        }
+    }
+
+    delete[] numbers;
+
+    return 0;
+}
