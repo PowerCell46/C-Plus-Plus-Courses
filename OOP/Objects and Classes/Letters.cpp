b90c9f364f790b30fa713bf758b59ba64df8a571
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2025-03-01 00:23:27 +0200

    Update Letters.cpp

--- OOP/Objects and Classes/Letters.cpp
+++ OOP/Objects and Classes/Letters.cpp
@@ -8,55 +8,78 @@
 #define EMPTY_CONTENT "---"
 
 
-int main() {
-    std::string text;
-    std::getline(std::cin, text);
+// Singleton Class with mainLogic() method
+class Letters {
+    Letters() = default;
 
-    std::stringstream textLineStream{text};
-    std::map<std::string, std::set<char>> wordsMap;
+    ~Letters() = default;
 
-    std::string currentWord;
-    while (textLineStream >> currentWord) {
-        auto currentWordIter = currentWord.cbegin();
-        std::set<char> currentWordSet;
-        std::stringstream currentWordStream{};
+public:
+    void mainLogic() {
+        std::string text;
+        std::getline(std::cin, text);
 
-        while (currentWordIter != currentWord.cend()) {
-            if (const auto currentChar = std::tolower(*currentWordIter); currentChar >= 'a' && currentChar <= 'z') {
-                currentWordStream << *currentWordIter;
-                currentWordSet.insert(currentChar);
-            } else {
-                wordsMap.insert({currentWordStream.str(), currentWordSet});
-                currentWordSet.clear();
-                currentWordStream.str("");
+        std::stringstream textLineStream{text};
+        std::map<std::string, std::set<char> > wordsMap;
+
+        std::string currentWord;
+        while (textLineStream >> currentWord) {
+            auto currentWordIter = currentWord.cbegin();
+            std::set<char> currentWordSet;
+            std::stringstream currentWordStream{};
+
+            while (currentWordIter != currentWord.cend()) {
+                if (const auto currentChar = std::tolower(*currentWordIter); currentChar >= 'a' && currentChar <= 'z') {
+                    currentWordStream << *currentWordIter;
+                    currentWordSet.insert(currentChar);
+                } else {
+                    wordsMap.insert({currentWordStream.str(), currentWordSet});
+                    currentWordSet.clear();
+                    currentWordStream.str("");
+                }
+                ++currentWordIter;
             }
-            ++currentWordIter;
+            if (!currentWord.empty())
+                wordsMap.insert({currentWordStream.str(), currentWordSet});
         }
-        if (!currentWord.empty())
-            wordsMap.insert({currentWordStream.str(), currentWordSet});
+
+        while (true) {
+            char currentChar;
+            std::cin >> currentChar;
+            if (currentChar == DOT)
+                break;
+
+            std::set<std::string> filteredWords;
+
+            for (const auto &word_map: wordsMap) {
+                if (word_map.second.find(std::tolower(currentChar)) != word_map.second.end())
+                    filteredWords.insert(word_map.first);
+            }
+
+            if (filteredWords.empty())
+                std::cout << EMPTY_CONTENT << '\n';
+            else {
+                for (auto &filteredWord: filteredWords)
+                    std::cout << filteredWord << ' ';
+                std::cout << '\n';
+            }
+        }
     }
 
-    while (true) {
-        char currentChar;
-        std::cin >> currentChar;
-        if (currentChar == DOT)
-            break;
+    Letters(const Letters&) = delete;
 
-        std::set<std::string> filteredWords;
+    Letters& operator=(const Letters&) = delete;
 
-        for (const auto& word_map : wordsMap) {
-            if (word_map.second.find(std::tolower(currentChar)) != word_map.second.end())
-                filteredWords.insert(word_map.first);
-        }
-
-        if (filteredWords.empty())
-            std::cout << EMPTY_CONTENT << '\n';
-        else {
-            for (auto& filteredWord : filteredWords)
-                std::cout << filteredWord << ' ';
-            std::cout << '\n';
-        }
+    static Letters& getInstance() {
+        static Letters instance;
+        return instance;
     }
+};
 
+int main() {
+    Letters& mainInstance = Letters::getInstance();
+
+    mainInstance.mainLogic();
+    
     return 0;
 }
