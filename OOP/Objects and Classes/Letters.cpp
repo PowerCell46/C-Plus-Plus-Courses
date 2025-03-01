e07706c489bf355ef264a924ef9eeeb975caaa5c
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2025-03-01 11:29:27 +0200

    Update Letters.cpp

--- OOP/Objects and Classes/Letters.cpp
+++ OOP/Objects and Classes/Letters.cpp
@@ -7,22 +7,15 @@
 #define DOT '.'
 #define EMPTY_CONTENT "---"
 
-
-// Singleton Class with mainLogic() method
+// Singleton class
 class Letters {
     Letters() = default;
 
     ~Letters() = default;
 
-public:
-    void mainLogic() {
-        std::string text;
-        std::getline(std::cin, text);
-
-        std::stringstream textLineStream{text};
-        std::map<std::string, std::set<char> > wordsMap;
-
+    void readAndAppendOnlyWords(std::map<std::string, std::set<char>>& wordsMap, std::stringstream& textLineStream) {
         std::string currentWord;
+
         while (textLineStream >> currentWord) {
             auto currentWordIter = currentWord.cbegin();
             std::set<char> currentWordSet;
@@ -32,32 +25,52 @@ public:
                 if (const auto currentChar = std::tolower(*currentWordIter); currentChar >= 'a' && currentChar <= 'z') {
                     currentWordStream << *currentWordIter;
                     currentWordSet.insert(currentChar);
+
                 } else {
                     wordsMap.insert({currentWordStream.str(), currentWordSet});
                     currentWordSet.clear();
                     currentWordStream.str("");
                 }
+
                 ++currentWordIter;
             }
+
             if (!currentWord.empty())
                 wordsMap.insert({currentWordStream.str(), currentWordSet});
         }
+    }
+
+    void filterWords(const std::map<std::string, std::set<char>>& wordsMap, const char& currentChar, std::set<std::string>& filteredWords) {
+        for (const auto &word_map: wordsMap) {
+            if (word_map.second.find(std::tolower(currentChar)) != word_map.second.end())
+                filteredWords.insert(word_map.first);
+        }
+    }
+
+public:
+    void mainLogic() {
+        std::string text;
+        std::getline(std::cin, text);
+
+        std::stringstream textLineStream{text};
+        std::map<std::string, std::set<char> > wordsMap;
+
+        readAndAppendOnlyWords(wordsMap, textLineStream);
 
         while (true) {
             char currentChar;
             std::cin >> currentChar;
+
             if (currentChar == DOT)
                 break;
 
             std::set<std::string> filteredWords;
 
-            for (const auto &word_map: wordsMap) {
-                if (word_map.second.find(std::tolower(currentChar)) != word_map.second.end())
-                    filteredWords.insert(word_map.first);
-            }
+            filterWords(wordsMap, currentChar, filteredWords);
 
             if (filteredWords.empty())
                 std::cout << EMPTY_CONTENT << '\n';
+
             else {
                 for (auto &filteredWord: filteredWords)
                     std::cout << filteredWord << ' ';
@@ -76,10 +89,11 @@ public:
     }
 };
 
+
 int main() {
     Letters& mainInstance = Letters::getInstance();
 
     mainInstance.mainLogic();
-    
+
     return 0;
 }
