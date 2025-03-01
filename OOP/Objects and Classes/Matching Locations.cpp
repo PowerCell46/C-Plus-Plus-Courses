bbddd4150f29bc607cf1437b4eea9dfe119e29d0
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2025-03-01 11:59:41 +0200

    Update Matching Locations.cpp

--- OOP/Objects and Classes/Matching Locations.cpp
+++ OOP/Objects and Classes/Matching Locations.cpp
@@ -2,45 +2,79 @@
 #include <vector>
 #include <sstream>
 
+#define DOT_STR "."
 
-int main() {
-    std::vector<std::string> matchingLocations;
 
-    while (true) {
-        std::string currentLine;
-        std::getline(std::cin, currentLine);
+class MatchingLocations {
+    MatchingLocations() = default;
 
-        if (currentLine == ".")
-            break;
-        matchingLocations.push_back(currentLine);
+    ~MatchingLocations() = default;
+
+    std::vector<std::string> initMatchingLocations() {
+        std::vector<std::string> matchingLocations;
+
+        while (true) {
+            std::string currentLine;
+            std::getline(std::cin, currentLine);
+
+            if (currentLine == DOT_STR)
+                break;
+            matchingLocations.push_back(currentLine);
+        }
+
+        return matchingLocations;
     }
 
-    while (true) {
-        std::string currentLine;
-        std::getline(std::cin, currentLine);
+    void searchByCityName(const std::vector<std::string> &matchingLocations, const std::string &cityName) {
+        for (const auto &matchingLocation: matchingLocations)
+            if (matchingLocation.substr(0, cityName.size()) == cityName)
+                std::cout << matchingLocation << '\n';
+    }
 
-        if (currentLine == ".")
-            break;
+    void searchByCoordinates(const std::vector<std::string> &matchingLocations, std::string &coordinates) {
+        coordinates.replace(coordinates.find(' '), 1, ",");
 
-        if (const auto firstChar = std::tolower(currentLine.at(0)); firstChar >= 'a' && firstChar <= 'z')
-            // If the first char is from a-z -> it's a town name
-            for (const auto &matchingLocation: matchingLocations) {
-                if (matchingLocation.substr(0, currentLine.size()) == currentLine)
-                    std::cout << matchingLocation << '\n';
+        for (const auto &matchingLocation: matchingLocations)
+            if (
+                coordinates.size() < matchingLocation.size() &&
+                matchingLocation.substr(matchingLocation.size() - coordinates.size()) == coordinates
+            )
+                std::cout << matchingLocation << '\n';
+    }
+
+public:
+    void mainLogic() {
+        std::vector<std::string> matchingLocations = initMatchingLocations();
+
+        while (true) {
+            std::string currentLine;
+            std::getline(std::cin, currentLine);
+
+            if (currentLine == DOT_STR)
+                break;
+
+            if (const auto firstChar = std::tolower(currentLine.at(0)); firstChar >= 'a' && firstChar <= 'z')
+                searchByCityName(matchingLocations, currentLine);
+            else {
+                searchByCoordinates(matchingLocations, currentLine);
             }
-
-        else {
-            // If the first char is ! from a-z -> It's latitude and longitude 
-            currentLine.replace(currentLine.find(' '), 1, ","); // replace the space with ','
-
-            for (const auto &matchingLocation: matchingLocations)
-                if (
-                        currentLine.size() < matchingLocation.size() &&
-                        matchingLocation.substr(matchingLocation.size() - currentLine.size()) == currentLine
-                    )
-                    std::cout << matchingLocation << '\n';
         }
     }
+
+    MatchingLocations(const MatchingLocations &) = delete;
+
+    MatchingLocations &operator=(const MatchingLocations &) = delete;
+
+    static MatchingLocations &getInstance() {
+        static MatchingLocations instance;
+        return instance;
+    }
+};
+
+int main() {
+    MatchingLocations& matchingLocations = MatchingLocations::getInstance();
+
+    matchingLocations.mainLogic();
     
     return 0;
 }
